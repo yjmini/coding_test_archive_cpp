3d8cbc5b886a4f6bed15f0c8d000fbfc55f7d489
yjmini <74178991+yjmini@users.noreply.github.com>
2026-04-02 21:50:05 +0900

    [Bronze V] Title: 아!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/4999. 아！/아！.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string str1, str2;
+	cin >> str1 >> str2;
+
+	if (str1.length() >= str2.length())
+		cout << "go";
+	else
+		cout << "no";
+
+	return 0;
+}

177f04c919acc225ba8e485902688e30ce0227fd
yjmini <74178991+yjmini@users.noreply.github.com>
2026-04-23 17:17:10 +0900

    [Bronze V] Title: 상근이의 친구들, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5717. 상근이의 친구들/상근이의 친구들.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	while (true)
+	{
+		int m, f;
+		cin >> m >> f;
+
+		if (m == 0 && f == 0)
+			break;
+
+		cout << m + f << '\n';
+	}
+
+
+	return 0;
+}
